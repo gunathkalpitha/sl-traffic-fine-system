60131e76241fbf1fed7da153461b93faf6211120
Pasinduteshan7 <eg245365@engug.ruh.ac.lk>
2026-07-24 10:54:24 +0530

    Commit 3: Update Repositories for new entity relationships

---
+++ Backend/src/main/java/com/Backend/repository/FineRepository.java
@@ -0,0 +1,13 @@
+package com.Backend.repository;
+
+
+import com.Backend.entity.Fine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface FineRepository
+        extends JpaRepository<Fine, Long> {
+
+    // Find fine by reference number
+    Optional<Fine> findByReferenceNumber(String referenceNumber);
+}
